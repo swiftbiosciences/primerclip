d9808be228bdee2f77d8d42fb1c3e8a7545fe801
Jonathan Irish <irish@swiftbiosci.com>
2018-11-15 10:06:33 -0500

    cabal tweaks

--- app/Main.hs
+++ app/Main.hs
@@ -23,7 +23,7 @@ main = do
             (fullDesc <> progDesc
                         "Trim PCR primer sequences from aligned reads"
                       <> header
-                        "primerclip -- Swift Biosciences Accel-Amplicon targeted panel primer trimming tool v0.3.3")
+                        "primerclip -- Swift Biosciences Accel-Amplicon targeted panel primer trimming tool v0.3.4")
     args <- execParser opts
     runstats <- runPrimerTrimming args
     putStrLn "primer trimming complete."
